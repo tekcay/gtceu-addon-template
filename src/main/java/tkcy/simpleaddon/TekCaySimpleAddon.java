4c45897ad7c4110beb35b3d43e1ba2c50ad13e8a
pierre <pierreblc@hotmail.fr>
2023-11-28 12:08:22 +0100

    still refacto2

--- src/main/java/tkcy/simpleaddon/TekCaySimpleAddon.java
+++ src/main/java/tkcy/simpleaddon/TekCaySimpleAddon.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.jetbrains.annotations.NotNull;
+import tkcy.simpleaddon.api.TKCYSAInternalTags;
 import tkcy.simpleaddon.api.utils.TKCYSALog;
 import tkcy.simpleaddon.common.CommonProxy;
 import tkcy.simpleaddon.common.block.TKCYSAMetaBlocks;
@@ -13,16 +14,16 @@ import tkcy.simpleaddon.common.metatileentities.TKCYSAMetaTileEntities;
 @Mod(modid = TekCaySimpleAddon.MODID,
      name = TekCaySimpleAddon.NAME,
      version = TekCaySimpleAddon.VERSION,
-     dependencies = GTInternalTags.DEP_VERSION_STRING)
+     dependencies = TKCYSAInternalTags.DEP_VERSION_STRING)
 public class TekCaySimpleAddon {
 
     public static final String MODID = "tkcysa";
     public static final String NAME = "TeK CaY Simple Addon";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = tkcy.TKCYSAInternalTags.VERSION;
 
     @SidedProxy(modId = MODID,
-                clientSide = "gregicality.multiblocks.common.ClientProxy",
-                serverSide = "gregicality.multiblocks.common.CommonProxy")
+                clientSide = "tkcy.simpleaddon.common.ClientProxy",
+                serverSide = "tkcy.simpleaddon.common.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
